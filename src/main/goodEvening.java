f66965102cab0aa9f1ccc53dcf47567c9365eb63
handan.tekin85@gmail.com <handan.tekin85@gmail.com>
2018-03-10 11:21:28 -0600

    added print statement

--- src/main/goodEvening.java
+++ src/main/goodEvening.java
@@ -3,6 +3,8 @@ package main;
 public class goodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class");
+		
+		System.out.println("My name is Melis");
 	}
 
 }
